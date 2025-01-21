2131eac0c43e43377af87aad4ba6d59c1334427d
Olympique13 <rayan.hamdaoui@charlespeguymarseille.com>
2025-01-21 14:05:16 +0100

    Page et formulaire de contact fonctionnelle (sans mail)

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class PageController extends AbstractController
 {
@@ -12,5 +16,22 @@ final class PageController extends AbstractController
     public function index(): Response
     {
         return $this->render('page/homepage.html.twig', array());
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($contact);
+            $entityManager->flush();
+        }
+
+        return $this->render('page/contact.html.twig', [
+            'contactForm' => $form->createView()
+        ]);
     }
 }
