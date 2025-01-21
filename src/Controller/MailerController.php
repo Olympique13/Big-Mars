459c5d62114c33ad4cf4cef63fa90d4bdea6c562
Olympique13 <rayan.hamdaoui@charlespeguymarseille.com>
2025-01-21 16:50:24 +0100

    Envoi de mail avec les infos du form de contact, notification lorsqu'il a été envoyé

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -5,20 +5,23 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 
 class MailerController extends AbstractController
 {
     #[Route('/mailer', name: 'app_mailer')]
-    public function sendEmail(MailerInterface $mailer)
+    public function sendEmail(MailerInterface $mailer, Request $request)
     {
+        
+        
         $email = (new Email())
             ->from($this->getParameter('EMAIL_FROM'))
             ->to($this->getParameter('EMAIL_TO'))
-            ->subject('Your subject here')
-            ->text('This is the text version of the email.')
-            ->html('<p>This is the HTML version of the email.</p>');
+            ->subject($this->getParameter('EMAIL_SUBJECT'))
+            ->text($this->getParameter('EMAIL_CONTENT'))
+            ->html($this->getParameter('EMAIL_CONTENT'));
 
         $mailer->send($email);
 
--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Form\ContactType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,7 +21,7 @@ final class PageController extends AbstractController
     }
 
     #[Route('/contact', name: 'app_contact')]
-    public function contact(Request $request, EntityManagerInterface $entityManager): Response
+    public function contact(Request $request, EntityManagerInterface $entityManager, MailerInterface $mailer): Response
     {
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
@@ -28,8 +30,24 @@ final class PageController extends AbstractController
         // dd($this->getParameter('DEMO'));
         
         if ($form->isSubmitted() && $form->isValid()) {
+            $this->addFlash('notice','Message envoyé');
             $entityManager->persist($contact);
             $entityManager->flush();
+            $email = (new TemplatedEmail())
+                ->from($form->get('email')->getData())
+                ->to('neyssimodeur@gmail.com')
+                ->subject($form->get('subject')->getData())
+                ->text($form->get('content')->getData())
+                ->htmlTemplate('page/email.html.twig')
+                ->context([
+                    'firstName'=> $form->get('firstName')->getData(),
+                    'lastName'=> $form->get('lastName')->getData(),
+                    'phone'=> $form->get('phone')->getData(),
+                    'subject'=> $form->get('subject')->getData(),
+                    'content'=> $form->get('content')->getData()
+                ]);
+              
+                $mailer->send($email);
         }
 
         return $this->render('page/contact.html.twig', [
