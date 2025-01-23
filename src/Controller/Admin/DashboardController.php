66c5fd2786c98391f76a2384739052ddaec78634
Olympique13 <rayan.hamdaoui@charlespeguymarseille.com>
2025-01-23 10:45:54 +0100

    retour accueil depuis admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -8,15 +8,12 @@ use App\Entity\Category;
 use App\Entity\Event;
 use App\Entity\EventRegistration;
 use App\Entity\Place;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 
 #[IsGranted('ROLE_ADMIN', message: 'Vous n\'êtes pas autorisé à consulter cette page')]
 class DashboardController extends AbstractDashboardController
@@ -39,7 +36,7 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('DashboPage d\'accueil', 'fa fa-home');
+        yield MenuItem::linkToUrl('Retour à la page d\'accueil', 'fas fa-home', $this->generateUrl('app_homepage'));
         yield MenuItem::linkToCrud('Catégories', 'fas fa-layer-group', Category::class);
         yield MenuItem::linkToCrud('Emplacements', 'fas fa-location-dot', Place::class);
         yield MenuItem::linkToCrud('Evènements', 'fas fa-calendar-days', Event::class);
