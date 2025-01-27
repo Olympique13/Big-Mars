436be485f9419c13c6a10c575f774237a1d26044
Olympique13 <rayan.hamdaoui@charlespeguymarseille.com>
2025-01-27 15:12:26 +0100

    Cleave phone, fix affichage après refresh

--- public/contactForm.js
+++ public/contactForm.js
@@ -3,12 +3,18 @@ document.addEventListener( 'DOMContentLoaded', function () {
     var companyGroup = document.querySelector( '#company-group' );
     var talentGroup = document.querySelector( '#talent-group' );
 
-
-    typeElements.forEach((item) => {
-        if (item.checked) {
-            console.log( item );
+    typeElements.forEach( ( item ) => {
+        if ( item.checked ) {
+            console.log( item.value );
+            if ( item.value === 'Talent' ) {
+                talentGroup.style.display = "block";
+                companyGroup.style.display = "none";
+            } else if ( item.value === 'Entreprise' ) {
+                companyGroup.style.display = "block";
+                talentGroup.style.display = "none";
+            }
         }
-    })
+    } )
 
     typeElements.forEach( function ( item ) {
         item.addEventListener( 'change', function ( e ) {
