aa2aae6668e628fa19ac3e1b23b3aca86dd42c15
baptsynergie <baptiste.gauthier@synergiefamily.com>
2025-02-04 11:46:34 +0100

    ajout brevo newsletter ok

--- assets/js/newsletter.js
+++ assets/js/newsletter.js
@@ -4,10 +4,23 @@ newsForms.forEach(item => {
     item.addEventListener("submit" , (e) => {
         e.preventDefault();
         const inputs = e.target.elements;
+        const successMsg = e.target.querySelector('.js-success-message');
         const inputEmail = inputs["email"];
         const email = inputEmail.value;
 
-        fetchEmail(email).then(r => console.log(r));
+        fetchEmail(email).then(r => {
+            let message = r.message;
+            successMsg.style.opacity = '1';
+            successMsg.append(message);
+            inputEmail.value = '';
+            setTimeout(() => {
+                successMsg.style.opacity = '0';
+                setTimeout(() => {
+                    successMsg.innerHTML = '';
+                }, 1000)
+            }, 3000);
+
+        });
 
     })
 })
--- src/Service/BrevoService.php
+++ src/Service/BrevoService.php
@@ -5,6 +5,7 @@ namespace App\Service;
 use Brevo\Client\Api\ContactsApi;
 use Brevo\Client\Configuration;
 use Brevo\Client\Model\AddContactToList;
+use Brevo\Client\Model\CreateContact;
 use GuzzleHttp\Client;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
@@ -18,18 +19,22 @@ class BrevoService {
         $this->config = Configuration::getDefaultConfiguration()->setApiKey('api-key' ,$this->brevoApiKey);
     }
 
-    public function addContact(string $email) {
+    public function addContact(string $email): JsonResponse
+    {
 
         $apiInstance = new ContactsApi(
             new Client(),
             $this->config
         );
 
-        $contactIdentifiers = new AddContactToList();
-        $contactIdentifiers['emails'] = array($email);
+        $createContact = new CreateContact();
+        $createContact['email'] = $email;
+        $createContact['listIds'] = [$this->listId];
 
         try {
-            
+
+            $apiInstance->createContact($createContact);
+
             return new JsonResponse([
                 'code' => 200,
                 'message' => 'Merci ! Votre inscription à bien était prise en compte.'
@@ -37,12 +42,10 @@ class BrevoService {
 
         } catch (\Exception $e) {
 
-            echo 'Exception when calling ContactsApi->addContactToList: ', $e->getMessage(), PHP_EOL;
-
             return new JsonResponse([
                 'code' => 500,
                 'message' => 'Une erreur est survenu, veuillez essayer plus tard.',
-                'error' => 'Exception when calling ContactsApi->addContactToList: ' . $e->getMessage(), PHP_EOL,
+                'error' => 'Exception when calling ContactsApi->createContact: ' . $e->getMessage(), PHP_EOL,
             ]);
         }
 
