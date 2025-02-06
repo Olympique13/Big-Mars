c58c57e7ffa3071959e294f9bc346fdaeba04ac6
Olympique13 <rayan.hamdaoui@charlespeguymarseille.com>
2025-02-06 14:53:46 +0100

    BO : filtre de recherche EventRegistrationCrud et ContactCrud + export csv avec nom de fichier selon le filtre

--- src/Controller/Admin/ContactCrudController.php
+++ src/Controller/Admin/ContactCrudController.php
@@ -3,15 +3,21 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Contact;
+use App\Service\CsvExporter;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
+use EasyCorp\Bundle\EasyAdminBundle\Factory\FilterFactory;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\ChoiceFilter;
 
 class ContactCrudController extends AbstractCrudController
 {
@@ -22,6 +28,7 @@ class ContactCrudController extends AbstractCrudController
 
     public function configureCrud(Crud $crud): Crud
     {
+        
         return $crud
         ->setDefaultSort(['createdAt' => 'DESC'])
         ->setPaginatorPageSize(15)
@@ -29,13 +36,42 @@ class ContactCrudController extends AbstractCrudController
         ->setPageTitle('index', 'Message de contact');
     }
 
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add(ChoiceFilter::new('type')->setChoices([
+                'Joueur' => 'joueur',
+                'Entreprise' => 'entreprise',
+            ]));
+    }
+    
     public function configureActions(Actions $actions): Actions
     {
+        $csvExportAction = Action::new('export', 'Export CSV', 'fa fa-download')
+        ->linkToCrudAction('export')
+        ->createAsGlobalAction();
+        
         return parent::configureActions($actions)
             ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->add(Crud::PAGE_INDEX, $csvExportAction)
             ->disable(Action::NEW)
             ->disable(Action::EDIT)
             ->disable(Action::DELETE);
+    }
+
+    public function export(AdminContext $context, CsvExporter $csvExporter)
+    {
+        $fields = FieldCollection::new($this->configureFields(Crud::PAGE_INDEX));
+        $filters = $this->container->get(FilterFactory::class)->create($context->getCrud()->getFiltersConfig(), $fields, $context->getEntity());
+        $queryBuilder = $this->createIndexQueryBuilder($context->getSearch(), $context->getEntity(), $fields, $filters);
+
+        $filters = $context->getRequest()->query->all('filters');
+
+        $filterValue = $filters['type']['value'] ?? 'Global';
+        // dd($filterValue);
+
+        // $filterValue correspond au nom de la valeur de recherche mise dans le filtre (joueur ou entreprise)
+        return $csvExporter->createResponseFromQueryBuilder($queryBuilder, $fields, 'contactExport_' . $filterValue . '.csv');
     }
 
     public function configureFields(string $pageName): iterable
--- src/Controller/Admin/EventRegistrationCrudController.php
+++ src/Controller/Admin/EventRegistrationCrudController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller\Admin;
 
+use App\Filter\EventNameFilter;
 use App\Entity\EventRegistration;
+use App\Service\CsvExporter;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
@@ -11,11 +13,12 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
+use EasyCorp\Bundle\EasyAdminBundle\Factory\FilterFactory;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use App\Filter\EventNameFilter;
 
 class EventRegistrationCrudController extends AbstractCrudController
 {
@@ -26,11 +29,31 @@ class EventRegistrationCrudController extends AbstractCrudController
 
     public function configureActions(Actions $actions): Actions
     {
+        $csvExportAction = Action::new('export', 'Export CSV', 'fa fa-download')
+            ->linkToCrudAction('export')
+            ->createAsGlobalAction();
+
         return parent::configureActions($actions)
             ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->add(Crud::PAGE_INDEX, $csvExportAction)
             ->disable(Action::NEW)
             ->disable(Action::EDIT)
             ->disable(Action::DELETE);
+    }
+
+    public function export(AdminContext $context, CsvExporter $csvExporter)
+    {
+        $fields = FieldCollection::new($this->configureFields(Crud::PAGE_INDEX));
+        $filters = $this->container->get(FilterFactory::class)->create($context->getCrud()->getFiltersConfig(), $fields, $context->getEntity());
+        $queryBuilder = $this->createIndexQueryBuilder($context->getSearch(), $context->getEntity(), $fields, $filters);
+
+        $filters = $context->getRequest()->query->all('filters');
+
+        $filterValue = $filters['event']['value'] ?? 'Global';
+        // dd($filterValue);
+
+        // $filterValue retourne l'id de l'événement selectionné dans le filtre sinon retourne Global
+        return $csvExporter->createResponseFromQueryBuilder($queryBuilder, $fields, 'inscriptionEvenement_' . $filterValue. '.csv');
     }
 
     public function configureFilters(Filters $filters): Filters
@@ -52,7 +75,6 @@ class EventRegistrationCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id')->hideOnForm()->hideOnIndex(),
             AssociationField::new('eventSlot', 'Créneaux'),
             TextField::new('eventSlot.event.title', 'Evénement'),
             TextField::new('firstName', 'Nom'),
